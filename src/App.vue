35cdfc4f6a436772726e8ad67b14fc91d6411799
Guilherme Augusto Vieira <guilhermeavdino@gmail.com>
2021-09-11 01:16:57 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <LogoAcquaman/>
+</template>
+
+<script>
+import LogoAcquaman from '@/assets/svg/logo_acquaman.svg';
+
+export default {
+  name: 'App',
+  components: {
+    LogoAcquaman,
+  }
+};
+</script>
+
+<style lang="scss">
+@import '@/scss/main.scss';
+</style>
